8ac1563e01a13eaa0e2dcdb7f819100fc7c216bb
shreyansh <shreyansh212004@gmail.com>
2025-06-29 19:33:03 +0530

    captain authentication done

--- Backend/middlewares/authmiddleware.js
+++ Backend/middlewares/authmiddleware.js
@@ -1,4 +1,5 @@
 const userModel = require("../models/user.model")
+const captainModel = require("../models/captain.model")
 const blackListModel = require("../models/blacklistToken.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -29,3 +30,30 @@ module.exports.authUser = async (req, res, next) => {
         res.status(400).send("ERROR "+error.message)
     }
 };
+
+
+module.exports.authCaptain = async(req,res,next)=>{
+    try {
+        const token =
+            req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+        if (!token) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        const isBlackListed = await blackListModel.findOne({ token: token });
+
+        if (isBlackListed) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        const captain = await captainModel.findById(decoded._id);
+
+        req.captain = captain;
+        return next();
+
+    } catch (error) {
+        res.status(400).send("ERROR " + error.message);
+    }
+}
--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
 const captainController = require("../controller/captain.controller");
+const authMiddleware = require("../middlewares/authmiddleware")
 
 
 
@@ -22,6 +23,16 @@ router.post('/register',[
     captainController.registerCaptain
 
 )
+
+router.post('/login',[
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("password").isLength({ min: 6 }).withMessage("Password must be atleast 6 charcters long"),
+],
+    captainController.loginCaptain
+)
+
+router.get('/profile',authMiddleware.authCaptain,captainController.getCaptainProfile)
+router.get('/logout',authMiddleware.authCaptain,captainController.logoutCaptain)
 
 
 module.exports = router;
