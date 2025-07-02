0dcbff16211520a5c94146d3b460873cc7cfc6a4
shreyansh <shreyansh212004@gmail.com>
2025-07-02 09:15:26 +0530

    User Home UI Done

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,11 +1,219 @@
-import React from 'react'
+import React, { useLayoutEffect, useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import "remixicon/fonts/remixicon.css";
+import LocationSearchPanel from "./components/LocationSearchPanel";
+import VehiclePanel from "./components/VehiclePanel";
+import ConfirmRide from "./components/ConfirmRide";
+import LookingForDriver from "./components/LookingForDriver";
+import WaitingForDriver from "./components/WaitingForDriver";
 
 const Home = () => {
-  return (
-    <div>
-      home
-    </div>
-  )
-}
+    const [pickup, setPickup] = useState("");
+    const [destination, setDestination] = useState("");
+    const [panelOpen, setPanelOpen] = useState(false);
+    const panelRef = useRef(null);
+    const panelClose = useRef(null);
+    const vehiclePanelRef = useRef(null);
+    const confirmRideRef = useRef(null);
+    const vehicleFoundRef = useRef(null);
+    const waitingForDriverRef = useRef(null);
+    const [vehiclePanel, setVehiclePanel] = useState(false);
+    const [confirmRidePanel, setConfirmRidePanel] = useState(false);
+    const [vehicleFound, setVehicleFound] = useState(false);
+    const [waitingForDriver, setWaitingForDriver] = useState(false);
 
-export default Home
+    const submitHandler = (e) => {
+        e.preventDefault();
+    };
+
+    useGSAP(
+        function () {
+            if (panelOpen) {
+                gsap.to(panelRef.current, {
+                    height: "70%",
+                    padding: 24,
+                });
+                gsap.to(panelClose.current, {
+                    opacity: 1,
+                });
+            } else {
+                gsap.to(panelRef.current, {
+                    height: "0%",
+                    padding: 0,
+                });
+                gsap.to(panelClose.current, {
+                    opacity: 0,
+                });
+            }
+        },
+        [panelOpen]
+    );
+
+    useGSAP(() => {
+        if (vehiclePanel) {
+            gsap.to(vehiclePanelRef.current, {
+                transform: "translateY(0)",
+
+            });
+        } else {
+            gsap.to(vehiclePanelRef.current, {
+                transform: "translateY(100%)",
+
+            });
+        }
+    }, [vehiclePanel]);
+
+    useGSAP(() => {
+        if (confirmRidePanel) {
+            gsap.to(confirmRideRef.current, {
+                transform: "translateY(0)",
+            });
+        } else {
+            gsap.to(confirmRideRef.current, {
+                transform: "translateY(100%)",
+            });
+        }
+    }, [confirmRidePanel]);
+
+    useGSAP(() => {
+        if (vehicleFound) {
+            gsap.to(vehicleFoundRef.current, {
+                transform: "translateY(0)",
+            });
+        } else {
+            gsap.to(vehicleFoundRef.current, {
+                transform: "translateY(100%)",
+            });
+        }
+    }, [vehicleFound]);
+
+    useGSAP(() => {
+        if (waitingForDriver) {
+            gsap.to(waitingForDriverRef.current, {
+                transform: "translateY(0)",
+            });
+        } else {
+            gsap.to(waitingForDriverRef.current, {
+                transform: "translateY(100%)",
+            });
+        }
+    }, [waitingForDriver]);
+
+
+    return (
+        <div className="h-screen relative overflow-hidden">
+            <img
+                className="w-16 absolute left-5 top-5"
+                src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+                alt="d"
+            />
+
+            {/* Location  */}
+            <div className="h-screen w-screen">
+                {/* Image for temporary use */}
+                <img
+                    className="h-full w-full object-cover"
+                    src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
+                    alt="map"
+                />
+            </div>
+
+            {/* Pick-Up Destination */}
+            <div className="  h-screen z-10 flex flex-col justify-end absolute top-0 w-full ">
+                <div className="h-[30%] p-5 bg-white relative ">
+                    <h5
+                        ref={panelClose}
+                        onClick={() => {
+                            setPanelOpen(false);
+                        }}
+                        className="absolute right-6 top-6 text-2xl"
+                    >
+                        <i className="ri-arrow-down-wide-fill"></i>
+                    </h5>
+
+                    <h4 className="text-2xl font-semibold">Find a trip</h4>
+                    <form
+                        onSubmit={(e) => {
+                            submitHandler(e);
+                        }}
+                    >
+                        <div className="line absolute h-16 w-1 top-[45%] left-10 bg-gray-900 rounded-full"></div>
+                        <input
+                            className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-5"
+                            type="text"
+                            placeholder="Add a pick-up location"
+                            value={pickup}
+                            onClick={() => {
+                                setPanelOpen(true);
+                            }}
+                            onChange={(e) => {
+                                setPickup(e.target.value);
+                            }}
+                        />
+                        <input
+                            className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-5"
+                            type="text"
+                            placeholder="Enter your destination"
+                            value={destination}
+                            onClick={() => {
+                                setPanelOpen(true);
+                            }}
+                            onChange={(e) => {
+                                setDestination(e.target.value);
+                            }}
+                        />
+                    </form>
+                </div>
+                <div ref={panelRef} className=" bg-white h-0">
+                    <LocationSearchPanel
+                        setPanelOpen={setPanelOpen}
+                        setVehiclePanel={setVehiclePanel}
+                    />
+                </div>
+            </div>
+
+            {/* Vehicle Pannel  */}
+            <div
+                ref={vehiclePanelRef}
+                style={{ transform: "translateY(100%)" }}
+                className="fixed w-full z-10 bottom-0 px-3 py-10 pt-12 translate-y-full bg-white"
+            >
+                <VehiclePanel
+                    setVehiclePanel={setVehiclePanel}
+                    setConfirmRidePanel={setConfirmRidePanel}
+                />
+            </div>
+
+            {/* Confirm Ride Pannel */}
+            <div
+                ref={confirmRideRef}
+                style={{ transform: "translateY(100%)" }}
+                className="fixed w-full z-10 bottom-0 px-3 py-6 pt-12 translate-y-full bg-white"
+            >
+                <ConfirmRide
+                    setConfirmRidePanel={setConfirmRidePanel}
+                    setVehicleFound={setVehicleFound}
+                />
+            </div>
+
+            <div
+                ref={vehicleFoundRef}
+                style={{ transform: "translateY(100%)" }}
+                className="fixed w-full z-10 bottom-0 px-3 py-6 pt-12 translate-y-full bg-white"
+            >
+                <LookingForDriver setVehicleFound={setVehicleFound} />
+            </div>
+
+            <div
+                ref={waitingForDriverRef}
+                style={{ transform: "translateY(100%)" }}
+                className="fixed w-full z-10 bottom-0 px-3 py-6 pt-12  bg-white"
+            >
+                <WaitingForDriver setWaitingForDriver={setWaitingForDriver} />
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ frontend/src/components/VehiclePanel.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+
+const VehiclePanel = (props) => {
+    return (
+        <div>
+            <h5
+                onClick={() => {
+                    props.setVehiclePanel(false);
+                }}
+                className="p-1 text-center absolute top-0 w-[93%]"
+            >
+                <i className="text-3xl text-gray-200  ri-arrow-down-wide-fill"></i>
+            </h5>
+            <h3 className="font-semibold text-2xl mb-5">Choose a Vehicle</h3>
+
+            <div
+                onClick={() => {
+                    props.setConfirmRidePanel(true);
+                    props.setVehiclePanel(false);
+                }}
+                className="flex w-full p-3 items-center mb-2 justify-between border-2 border-[#eee] active:border-black rounded-xl"
+            >
+                <img
+                    className="h-12"
+                    src="https://www.pngplay.com/wp-content/uploads/8/Uber-PNG-Photos.png"
+                    alt="Car"
+                />
+                <div className="w-1/2 ml-2">
+                    <h4 className="font-medium text-base">
+                        UberGo
+                        <span>
+                            <i className="ri-user-3-fill"></i>4
+                        </span>
+                    </h4>
+                    <h5 className="font-medium text-sm">2 mins away </h5>
+                    <p className="font-normal text-xs">
+                        Affordable, compact rides
+                    </p>
+                </div>
+                <h2 className="text-lg  font-semibold">₹193.20</h2>
+            </div>
+
+            <div
+                onClick={() => {
+                    props.setConfirmRidePanel(true);
+                    props.setVehiclePanel(false);
+                }}
+                className="flex w-full p-3 items-center mb-2 justify-between border-2 border-[#eee] active:border-black rounded-xl"
+            >
+                <img
+                    className="h-12"
+                    src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png"
+                    alt="Moto"
+                />
+                <div className="w-1/2 ml-2">
+                    <h4 className="font-medium text-base">
+                        Moto
+                        <span>
+                            <i className="ri-user-3-fill"></i>4
+                        </span>
+                    </h4>
+                    <h5 className="font-medium text-sm">2 mins away </h5>
+                    <p className="font-normal text-xs">
+                        Affordable, compact rides
+                    </p>
+                </div>
+                <h2 className="text-lg  font-semibold">₹193.20</h2>
+            </div>
+
+            <div
+                onClick={() => {
+                    props.setConfirmRidePanel(true);
+                    props.setVehiclePanel(false);
+                }}
+                className="flex border-2 border-[#eee] active:border-black  mb-2 rounded-xl w-full p-3  items-center justify-between"
+            >
+                <img
+                    className="h-12"
+                    src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png"
+                    alt="Auto"
+                />
+                <div className="w-1/2 ml-2">
+                    <h4 className="font-medium text-base">
+                        UberAuto
+                        <span>
+                            <i className="ri-user-3-fill"></i>4
+                        </span>
+                    </h4>
+                    <h5 className="font-medium text-sm">2 mins away </h5>
+                    <p className="font-normal text-xs">
+                        Affordable, compact rides
+                    </p>
+                </div>
+                <h2 className="text-lg  font-semibold">₹193.20</h2>
+            </div>
+        </div>
+    );
+};
+
+export default VehiclePanel;
